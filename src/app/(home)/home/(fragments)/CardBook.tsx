7607c74487ccd2acb7380969b4fbcdd40b52719b
Sofrosine <soultanmuh@gmail.com>
2024-03-29 18:33:36 +0700

    add point handler

--- src/app/(home)/home/(fragments)/CardBook.tsx
+++ src/app/(home)/home/(fragments)/CardBook.tsx
@@ -1,4 +1,8 @@
+"use client";
+
 import Button from "@/components/Button";
+import useAuthStore from "@/store/authStore";
+import { useErrorStore } from "@/store/errorStore";
 import Image from "next/image";
 import React, { FC } from "react";
 
@@ -8,6 +12,8 @@ type Props = {
 };
 
 const CardBook: FC<Props> = ({ book, onOrder }) => {
+  const { data } = useAuthStore();
+
   return (
     <div className="col-span-12 sm:col-span-6 md:col-span-12 lg:col-span-6 3xl:col-span-4 border border-grey-2 rounded-lg overflow-hidden">
       <div className="relative w-full h-[200px]">
@@ -23,7 +29,16 @@ const CardBook: FC<Props> = ({ book, onOrder }) => {
           </p>
         </div>
         {onOrder && (
-          <Button onClick={() => onOrder && onOrder()} className="w-full mt-8">
+          <Button
+            onClick={() => {
+              if (data?.user?.points! < book?.point) {
+                useErrorStore.getState().setMessage("Not enough point");
+              } else {
+                onOrder && onOrder();
+              }
+            }}
+            className="w-full mt-8"
+          >
             Order
           </Button>
         )}
